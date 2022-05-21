aa1cfd66db20fdf4af019dddecb855f352efd2a8
Dima Shirkov <dimashirkov@list.ru>
2022-05-21 23:16:44 +0300

    Daki draft v2

--- task.js
+++ task.js
@@ -1,15 +1,10 @@
 const menuLink = Array.from(document.getElementsByClassName('menu__link'));
-// const menuSub = document.querySelectorAll('.menu_sub')
-// console.log(menuSub);
 
 menuLink.forEach((element) => {
     if(element.nextElementSibling != null){
         element.onclick = function() {
             element.nextElementSibling.classList.add('menu_active');
+            return false;
         }
     }
-    // console.log(element.nextElementSibling != null);
-    // element.click = function() {
-    //     console.log(element.querySelector('menu menu_sub'));
-    // }
 })
