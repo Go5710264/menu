4b2549e2a7757e55906ec5a319229e57117dbf19
Dima Shirkov <dimashirkov@list.ru>
2022-05-26 06:52:12 +0300

    dak1 draft v2

--- task.js
+++ task.js
@@ -1,9 +1,17 @@
-const menuLink = Array.from(document.getElementsByClassName('menu__link'));
+const menuLink = Array.from(document.getElementsByClassName('menu__link')); // массив с ссылками навигационного меню 
+const menuItems = Array.from(document.getElementsByClassName('menu_sub')); // массив с из .menu_sub
+
 
 menuLink.forEach((element) => {
-    if(element.nextElementSibling != null){
+    if(element.nextElementSibling != null){ // если есть дочерний элемент
         element.onclick = function() {
-            element.nextElementSibling.classList.add('menu_active');
+            menuItems.forEach((activeElement) => {
+                if(activeElement.classList.contains('menu_active')) {
+                    activeElement.classList.remove('menu_active');
+                }; // если есть дочерний элемент
+            })
+            // необходимо удалить menu_active у другого элемента
+            element.nextElementSibling.classList.add('menu_active'); // к дочернему элементу добавить menu_active
             return false;
         }
     }
