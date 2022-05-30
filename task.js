ca6d1ff9067d7045d006e7769f5a8ea3ea71b2d4
Dima Shirkov <dimashirkov@list.ru>
2022-05-30 05:55:49 +0300

    dak1 draft v5

--- task.js
+++ task.js
@@ -1,13 +1,14 @@
 const menuLink = Array.from(document.getElementsByClassName('menu__link')); // массив с ссылками навигационного меню (в том числе и вложенных)
 const menuItems = Array.from(document.getElementsByClassName('menu_sub')); // массив из списков вложенных элементов
 let indexNav; // индекс предыдущей ссылки навигационного меню
-
+ 
 menuLink.forEach((element, index) => { // итерация по ссылкам навигационного меню
     if(element.nextElementSibling != null){ // если есть дочерний элемент
         element.onclick = function() { // обработчик click ссылки навигационного меню
             // закрытие вложенного меню при повторном клике
             if (index === indexNav) { // если индекс предыдущей ссылки навигационного меню = нынешей 
                 element.nextElementSibling.classList.remove('menu_active'); // тогда у вложенного элемента удалить .menu_active
+                indexNav = null;  
                 return false; // завершение работы обработчика событий
             } else { // в остальных случаях
                 // удаление ранее открытых меню
